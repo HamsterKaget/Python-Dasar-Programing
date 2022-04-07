8558bac1cfc9ca12503420c3fea92f8e7d226396
HamsterKaget <radzaramdani21@gmail.com>
2022-04-07 18:17:45 +0700

    Komentar di Python

---
+++ 00. Komentar Python/Komentar.py
@@ -0,0 +1,15 @@
+# Dalam Python , Komentar diberi awalan Hastag [ # ]
+
+# Hashtag digunakan untuk membuat singgle line comment dalam Python
+
+'''
+    3 petik Digunakan untuk
+    membuat multi line
+    comment
+    
+    jangan lupa untuk menutup multiline
+    comment dengan 3 petik ketika selesai
+'''
+
+# print('Hello World') <- code dalam comment tidak akan dijalankan
+print('Hello Python') # <- code disamping akan dijalankan
